e343fc1b0aeb095a491932335bd5f11775c2ec22
Dragonzslayer <dragonzslayer.mc@gmail.com>
2014-02-13 22:26:02 -0500

    Update Balance.java

--- src/Dragonzslayer/teamTag/Commands/Balance.java
+++ src/Dragonzslayer/teamTag/Commands/Balance.java
@@ -1,14 +1,23 @@
 package Dragonzslayer.teamTag.Commands;
 
+import java.util.Arrays;
+import java.util.List;
+
 import Dragonzslayer.teamTag.main;
+
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class Balance
 {
 	private main plugin = null;
-  public Balance(CommandSender sender, String[] args)
+  @SuppressWarnings({ "rawtypes", "deprecation" })
+public Balance(CommandSender sender, String[] args)
   {
 		this.plugin = main.getInstance();
 	  if (this.plugin.redTeam.size() > this.plugin.blueTeam.size()){
@@ -27,7 +36,29 @@ public class Balance
 				          if (this.plugin.deaths.containsKey(p.getName())) {
 				            this.plugin.deaths.remove(p.getName());
 				          }
-				          this.plugin.joinTeam.joinBlueTeam(p);
+				        Player player = p;
+				        player.sendMessage(this.plugin.pPrefix + ChatColor.BLUE + " You joined the Blue team!");
+				        player.sendMessage(this.plugin.pPrefix + ChatColor.GOLD + " To vote to start the game do: /tt vote");
+				        
+				        this.plugin.tagPlayers.add(player);
+				        this.plugin.blueTeam.add(player);
+
+				        int x = this.plugin.getConfig().getInt(player.getWorld().getName() + "." + "spectate" + "." + "X", player.getWorld().getSpawnLocation().getBlockX());
+				        int y = this.plugin.getConfig().getInt(player.getWorld().getName() + "." + "spectate" + "." + "Y", player.getWorld().getSpawnLocation().getBlockY());
+				        int z = this.plugin.getConfig().getInt(player.getWorld().getName() + "." + "spectate" + "." + "Z", player.getWorld().getSpawnLocation().getBlockZ());
+
+				        player.teleport(new Location(player.getWorld(), x, y, z));
+
+				        ItemStack[] inv = player.getInventory().getContents();
+				        List invList = Arrays.asList(inv);
+				        this.plugin.getConfig().set("Inventories." + player.getName(), invList);
+				        this.plugin.saveConfig();
+				        player.getInventory().clear();
+			            this.plugin.Methods.removeArmor(player);
+			            this.plugin.Methods.joinBlue(player);
+				        player.setGameMode(GameMode.ADVENTURE);
+				        player.getInventory().setItemInHand(new ItemStack(Material.SNOW_BALL, 64));
+				        player.updateInventory();
 				        }
 				  }
 			  }
@@ -49,7 +80,29 @@ public class Balance
 				          if (this.plugin.deaths.containsKey(p.getName())) {
 				            this.plugin.deaths.remove(p.getName());
 				          }
-				          this.plugin.joinTeam.joinRedTeam(p);
+				        Player player = p;
+				  		player.sendMessage(this.plugin.pPrefix + ChatColor.RED + " You joined the Red team!.");
+				        player.sendMessage(this.plugin.pPrefix + ChatColor.GOLD + " To vote to start the game do: /tt vote");
+				        
+				        this.plugin.tagPlayers.add(player);
+				        this.plugin.redTeam.add(player);
+
+				        int x = this.plugin.getConfig().getInt(player.getWorld().getName() + "." + "spectate" + "." + "X", player.getWorld().getSpawnLocation().getBlockX());
+				        int y = this.plugin.getConfig().getInt(player.getWorld().getName() + "." + "spectate" + "." + "Y", player.getWorld().getSpawnLocation().getBlockY());
+				        int z = this.plugin.getConfig().getInt(player.getWorld().getName() + "." + "spectate" + "." + "Z", player.getWorld().getSpawnLocation().getBlockZ());
+
+				        player.teleport(new Location(player.getWorld(), x, y, z));
+
+				        ItemStack[] inv = player.getInventory().getContents();
+				        List invList = Arrays.asList(inv);
+				        this.plugin.getConfig().set("Inventories." + player.getName(), invList);
+				        this.plugin.saveConfig();
+			            player.getInventory().clear();
+			            this.plugin.Methods.removeArmor(player);
+			            this.plugin.Methods.joinRed(player);
+				        player.setGameMode(GameMode.ADVENTURE);
+				        player.getInventory().setItemInHand(new ItemStack(Material.SNOW_BALL, 64));
+				        player.updateInventory();
 				        }
 				  }
 			  }
